cc09cc333dbee71ea8b0a4468989b526e5cd1842
Jake Salazar <jake.salazar@gmail.com>
2021-08-29 19:44:49 -0500

    removed cleanup script

--- tmp/cleanup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# So you don't have to manually delete these files
-#  everytime you destroy and provision this VM when testing stuff
-rm -rf .vagrant 
-# rm -rf html/*
-echo "all clean"
